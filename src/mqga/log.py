6ca1e578dacfc14e64ed4bfd77da62b074c51ef9
liuyu.fang <1725453672@qq.com>
2023-10-19 23:19:08 +0800

    回滚x

--- src/mqga/log.py
+++ src/mqga/log.py
@@ -4,10 +4,42 @@ import logging
 from time import strftime
 
 PATH = os.path.abspath('.') + '/log/'
-DATEFMT = '%Y-%m-%d %H:%M:%S'
 DEFAULT_LOGGER_NAME = "MQGA"
-Console_FMT = "\033[1;33m[%(levelname)s -> %(filename)s:%(lineno)s]\033[0m %(asctime)s: %(message)s"
-File_FMT = "[%(levelname)s -> %(filename)s:%(lineno)s] %(asctime)s: %(message)s"
+
+# \033[3xm x的取值范围0-7，分别对应下面几种颜色
+BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = range(8)
+
+COLORS = {
+    'WARNING': YELLOW,
+    'INFO': GREEN,
+    'DEBUG': BLUE,
+    'CRITICAL': RED,
+    'ERROR': RED
+}
+"""LOGLevel = {
+    0:'NOSET',
+    10:'DEBUG',
+    20:'INFO',
+    30:'WARNING',
+    40:'ERROR',
+    50:'CRITICAL'
+}"""
+
+DATEFMT = '%Y-%m-%d %H:%M:%S'
+Console_FMT = "\033[1;38m[\033[0m%(levelname)s \033[1;38m-> %(filename)s:%(lineno)s]\033[0m %(message)s"
+File_FMT = "[%(levelname)s -> %(filename)s:%(lineno)s] %(message)s"
+
+# 变更等级颜色
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, msg):
+        logging.Formatter.__init__(self, msg)
+
+    def format(self, record):
+        levelname = record.levelname
+        if levelname in COLORS:
+            levelname_color = f"\033[1;3{COLORS[levelname]}m{levelname}\033[0m"
+            record.levelname = levelname_color
+        return logging.Formatter.format(self, record)
 
 
 class MQGALog(object):
@@ -15,9 +47,9 @@ class MQGALog(object):
         if not os.path.exists(PATH):
             os.mkdir(PATH)
         self.logger = logging.getLogger(DEFAULT_LOGGER_NAME)
-        self.console_formatter = logging.Formatter(fmt=Console_FMT, datefmt=DATEFMT)
+        self.console_formatter = ColoredFormatter(Console_FMT)
         self.file_formatter = logging.Formatter(fmt=File_FMT, datefmt=DATEFMT)
-        self.log_filename = '{0}{1}.log'.format(PATH, strftime("%Y-%m-%d"))
+        self.log_filename = '{0}{1}.log'.format(PATH, strftime("%Y-%m-%d-%H"))
 
         self.logger.addHandler(self.get_file_handler(self.log_filename))
         self.logger.addHandler(self.get_console_handler())
