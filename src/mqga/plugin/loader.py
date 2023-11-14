e992081b56d0c0d5963bae587aa005d6f4961ec8
liggest <liggest@sina.cn>
2023-11-15 00:17:35 +0900

    暂时把插件根目录当做包来加载
    不知道这样好不好，但应该有助于避免命名冲突吧

--- src/mqga/plugin/loader.py
+++ src/mqga/plugin/loader.py
@@ -7,24 +7,27 @@ from pathlib import Path
 from mqga.log import log
 
 def load(root = Path("./plugin")) -> dict[Path, ModuleType]:
-    root_str = root.as_posix()
-    sys.path.insert(0, root_str)
-    log.info(f"从 {root_str} 中载入插件…")
+    if not (root / "__init__.py").exists():
+        raise ValueError(f"请在 {root!r} 中创建 __init__.py，使其变成一个 Python 包")
+    parent = root.parent
+    parent_abs = parent.as_posix()
+    sys.path.insert(0, parent_abs)
+    log.info(f"从 {root.as_posix()} 中载入插件…")
     plugins = {}
     for path in root.iterdir():
         if path.name.startswith("_"):
             continue
         if path.suffix.endswith(".py") or (path.is_dir() and (path / "__init__.py").exists()):
             try:
-                plugins[path] = load_one(path)
-                log.info(f"{path.as_posix()} 加载成功~")
+                plugins[path] = load_one(root, path)
+                log.info(f"插件 {path.as_posix()} 加载成功~")
             except Exception as e:
-                log.exception(f"{path.as_posix()} 加载失败！", exc_info=e)
-    if sys.path and sys.path[0] is root_str:
+                log.exception(f"插件 {path.as_posix()} 加载失败！", exc_info=e)
+    if sys.path and sys.path[0] is parent_abs:
         sys.path.pop(0)
         
-def load_one(path: Path) -> ModuleType:
+def load_one(root: Path, path: Path) -> ModuleType:
     # spec = spec_from_file_location(path.stem, path)
     # module = module_from_spec(spec)
     # spec.loader.exec_module(module)
-    return importlib.import_module(path.stem)
+    return importlib.import_module(f"{root.stem}.{path.stem}")
