525a700f683f8c47e7f0879f3c6fd2577aff5b15
liggest <liggest@sina.cn>
2023-12-05 02:56:49 +0900

    mqga 主项目热重载
    log 增殖了，还没搞清楚是咋回事

--- src/mqga/__init__.py
+++ src/mqga/__init__.py
@@ -1,30 +1,58 @@
 
 LEGACY = False
+RELOAD = False
 
 def _pre_defined():
-    global LEGACY
+    global LEGACY, RELOAD
     
     import argparse
     import sys
 
     parser = argparse.ArgumentParser(description="MQGA Pre-defined")
     parser.add_argument('-L', '--legacy', action='store_true', help='LEGACY')
+    parser.add_argument('-R', '--reload', action='store_true', help='开发模式下的热重载')
 
     args, rest = parser.parse_known_args()
     
     sys.argv = [sys.argv[0], *rest]
 
     LEGACY = args.legacy
+    RELOAD = args.reload
     if LEGACY:
-        print("【旧 频道模式】…")
+        from mqga.log import log
+        log.info("【旧 频道模式】…")
 
 _pre_defined()
 
-def run():
-    from mqga.bot import Bot
+if not RELOAD:
+    def run():
+        from mqga.bot import Bot
 
-    bot = Bot()
-    bot.run()
+        bot = Bot()
+        bot.run()
+else:
+    def run():
+        import sys
+        from mqga.log import log
+        
+        try:
+            import watchfiles
+        except ImportError:
+            log.error("或许当前不是开发环境，watchfiles 未安装，无法热重载")
+            sys.exit(1)
+        
+        from pathlib import Path
+        paths = [Path("./src/"), *Path(".").glob("*.py"), *Path(".").glob("*.toml")]
+        log.info("正在启用热重载…")
+        log.debug("\n".join(path.as_posix() for path in paths))
+        args = sys.argv
+        if args[0].endswith(".py"):  # 去掉 bot.py 等
+            args = args[1:]
+        watchfiles.run_process(*paths,
+            target=" ".join(("pdm", "bot", *args)), target_type="command"
+        )
+
+
 
 from typing import TYPE_CHECKING
 
