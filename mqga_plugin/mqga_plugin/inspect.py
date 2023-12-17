12bfdb7d4b63b53ff937d33c31f2e3fb090b2d86
liggest <liggest@sina.cn>
2023-12-18 03:18:08 +0900

    修复一些看起来不明不白的地方

--- mqga_plugin/mqga_plugin/inspect.py
+++ mqga_plugin/mqga_plugin/inspect.py
@@ -3,6 +3,7 @@ from mqga.lookup.context import context as ctx
 from mqga import on_message, group_only
 # from mqga.event.on import on_group_message as on_msg
 from mqga.q.message import ChannelMessage, GroupMessage
+from mqga.log import log
 
 @on_message.filter_by(lambda: ctx.message.content.lower().endswith("test"))
 def test():
@@ -28,3 +29,9 @@ with group_only:
             await asyncio.sleep(seconds)
             await bot._api.group.reply_text(str(seconds), payload)
         return delay()
+
+@on_message.full_match(r"501")
+def res501():
+    log.error("version:0.0.2\n/ping baidu.com -c 3\n/apikey xxx\n/apiping www.baidu.com -c 3")
+    return "version:0.0.2\n/ping baidu.com -c 3\n/apikey xxx\n/apiping www.baidu.com -c 3"
+    # 神秘内容，让官方接口返回 Http 501，而不是 json 格式的错误信息
