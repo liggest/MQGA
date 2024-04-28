a9931720c89656a53d63ff479f7cdff0a348fa59
liggest <liggest@sina.cn>
2024-04-29 03:11:11 +0900

    减少 py 的报错信息

--- mqga_plugin/mqga_plugin/test/inspect.py
+++ mqga_plugin/mqga_plugin/test/inspect.py
@@ -110,8 +110,12 @@ async def task():
     try:
         exec(script, env)
         result = await env["task"]()
-        if not isinstance(result, str):
-            result = repr(result)
-        return result
+
     except Exception as e:
-        return "\n".join(traceback.format_exception(e)).replace(".", "[.]")
+        result = e
+        store["ERROR"] = e
+        # return "\n".join(traceback.format_exception(e)).replace(".", "[.]")
+    if not isinstance(result, str):
+        result = repr(result)
+    result = result.replace(".", "[.]")
+    return result
