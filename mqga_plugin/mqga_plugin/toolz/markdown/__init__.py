9bccf1af744cc639695fcbf3b67d67381f22a60f
liggest <liggest@sina.cn>
2024-05-05 06:15:47 +0900

    toolz.markdown 文本交互快捷片段

--- mqga_plugin/mqga_plugin/toolz/markdown/__init__.py
+++ mqga_plugin/mqga_plugin/toolz/markdown/__init__.py
@@ -1,6 +1,6 @@
 
-from .markdown import by_template, by_content
+from .markdown import by_template, by_content, Snippet
 from . import keyboard
 from .config import register_markdown, register_keyboard
 
-__all__ = [ "by_template", "by_content", "keyboard", "register_markdown", "register_keyboard" ]
+__all__ = [ "by_template", "by_content", "Snippet", "keyboard", "register_markdown", "register_keyboard" ]
--- mqga_plugin/mqga_plugin/toolz/markdown/markdown.py
+++ mqga_plugin/mqga_plugin/toolz/markdown/markdown.py
@@ -4,6 +4,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from urllib.parse import quote
+
 if TYPE_CHECKING:
     from mqga.q.api import MarkdownTemplate, MarkdownCustom
 
@@ -20,3 +22,31 @@ def by_template(id: str, params: dict[str, str | list[str]] | None = None) -> Ma
             for k, v in params.items()
         ]
     return md
+
+class Snippet:
+    """ QQ markdown 文本交互快捷片段 """
+
+    @staticmethod
+    def at(user_id: str):
+        """ @user_id """
+        return f"<@{user_id}>"
+    
+    @staticmethod
+    def at_all():
+        """ @全体成员 """
+        return "@everyone"
+
+    @staticmethod
+    def inline_command(text: str, command: str, enter = False, reply = False):  # 也编码 "/"
+        """ MD 文本指令 """
+        return f"[{text}](mqqapi://aio/inlinecmd?command={quote(command, safe='')}&reply={str(reply).lower()}&enter={str(enter).lower()})"
+
+    @staticmethod
+    def to_channel(channel_id: str):
+        """ 跳转到 channel_id 对应的子频道 """
+        return f"<#{channel_id}>"
+    
+    @staticmethod
+    def emoji(emoji_id: str):
+        """ emoji_id 对应的系统表情 """
+        return f"<emoji:{emoji_id}>"
